61184de62f357c3bc5e084c64759b820cb79efac
Nate <126898702+4n0n3@users.noreply.github.com>
2024-02-07 09:31:47 -0500

    squashed scaling bugs with buttons and viewport

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5867587f86fc1b83"; }
+/******/ 		__webpack_require__.h = function() { return "c81588ce7ddd4e17"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
